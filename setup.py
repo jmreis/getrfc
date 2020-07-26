c501dedc11c1ea1f31d3aefa143b9b4788a06b36
Jair Reis <jairmsreis@gmail.com>
2020-07-26 17:00:01 -0300

    Added the CHANGELOG and MANIFEST.in

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="get_rfc",
+    name="getrfc",
     version="0.0.1",
     author="Jair reis",
     author_email="jmsrpython@protonmail.com",
     description="A simple module for get RFC",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jmreis/pygetrfc",
+    url="https://github.com/jmreis/getrfc",
     packages=setuptools.find_packages(),
     py_modules=['getrfc'],
     entry_points={
