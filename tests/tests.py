203c46196ee5f144b3f8edfe0414fa78c4d2849e
Jair Reis <jairmsreis@gmail.com>
2021-02-21 20:49:02 -0300

    Testing commit

--- tests/tests.py
+++ tests/tests.py
@@ -1,26 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
-# MIT License
-#
-# Copyright (c) 2020 Jair Reis <jmsrdebian@protonmail.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 """
 A simple module for get RFC from site www.ietf.org.
 
@@ -30,7 +9,10 @@ get your number and descripitions.
 """
 
 import sys
-import urllib.request
+from urllib.request import Request
+from urllib.request import urlopen
+from urllib.error import URLError
+from urllib.error import HTTPError
 
 
 __all__ = ['rfc_help', 'list_rfc', 'get_rfc']
@@ -276,7 +258,6 @@ def get_rfc():
     Tests:
     ------
     >>> get_rfc()
-
     """
     rfc_test = 20
     try:
@@ -288,7 +269,7 @@ def get_rfc():
     url = f'https://www.ietf.org/rfc/rfc{rfc_number}.txt'
     # Getting the RFC from url
     try:
-        rfc_raw = urllib.request.urlopen(url).read()
+        rfc_raw = urlopen(url).read()
         rfc = rfc_raw.decode()
         print(rfc)
     except HTTPError as e:
